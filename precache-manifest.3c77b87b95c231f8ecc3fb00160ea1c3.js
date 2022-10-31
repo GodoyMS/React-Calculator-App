0c3e7912c04d055392fcd5465ed71c6e8d532c8d
GodoyMS <youremail@domain.com>
2022-10-31 17:59:54 -0500

    Updates

---
+++ precache-manifest.3c77b87b95c231f8ecc3fb00160ea1c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "52fa233b624a79662d9f",
+    "url": "/react-calculator-app/static/css/main.07445a55.chunk.css"
+  },
+  {
+    "revision": "52fa233b624a79662d9f",
+    "url": "/react-calculator-app/static/js/main.52fa233b.chunk.js"
+  },
+  {
+    "revision": "69e0dab03ec9f6c99eb0",
+    "url": "/react-calculator-app/static/js/1.69e0dab0.chunk.js"
+  },
+  {
+    "revision": "bd67aa572db4c6c525f4",
+    "url": "/react-calculator-app/static/js/2.bd67aa57.chunk.js"
+  },
+  {
+    "revision": "1ff16beb8f0aa9fe06bb",
+    "url": "/react-calculator-app/static/js/runtime~main.1ff16beb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-calculator-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "71588d3057a20f0ff94c5fc6dc0ea983",
+    "url": "/react-calculator-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rReact-Calculator-Appeact-calculator-app/precache-manifest.bb8a866884c5d39c5b38d2e6380c42a2.js"
+  "/react-calculator-app/precache-manifest.3c77b87b95c231f8ecc3fb00160ea1c3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rReact-Calculator-Appeact-calculator-app/index.html", {
+workbox.routing.registerNavigationRoute("/react-calculator-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
